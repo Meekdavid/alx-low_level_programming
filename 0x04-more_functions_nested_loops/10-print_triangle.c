0c29845a832dd45a19a550e881cf9d7ed76485ca
Mboko David <98537263+Meekdavid@users.noreply.github.com>
2022-04-14 22:41:21 -0700

    try

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: input size
+ *
+ * Description: size of triangle depends on input
+ * Return: Always (0)
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
