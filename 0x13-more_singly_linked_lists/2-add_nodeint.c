ab6a5910e39501b890575813dd98d60b056bd9a6
Mboko David <98537263+Meekdavid@users.noreply.github.com>
2022-05-26 07:33:49 -0700

    correction

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,25 +1,27 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_nodeint - adding a new node at the beginning of the function
+ * nodeint - adding a new node at the beginning of the function
  * of a linked list or function
  * @head: head of a list or function
  * @n: n element.
  *
+ *This is David Mboko Function
  * Return: All addresses, if it is a new element
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-  listint_t *old;
+  listint_t *dave;
 
-  old = malloc(sizeof(listint_t));
+  dave = malloc(sizeof(listint_t));
 
-  if (old == NULL)
+  if (dave == NULL)
     return (NULL);
 
-  old->n = n;
-  old->next = *head;
-  *head = old;
+  dave->n = n;
+  dave->next = *head;
+  *head = dave;
 
   return (*head);
 }
