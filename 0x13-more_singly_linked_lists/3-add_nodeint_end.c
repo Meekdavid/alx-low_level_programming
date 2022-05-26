5d634df432723a03cfbda6a212984c608b341f88
Mboko David <98537263+Meekdavid@users.noreply.github.com>
2022-05-26 08:05:04 -0700

    correctuion

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,40 +1,33 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * add_nodeint_end - add a new node at the end
- * of a linked list
- * @head: head of a list.
- * @n: n element.
- *
- * Return: address of the new element. NUll if it failed.
- */
+ * add_nodeint_end - Add a new node at the end of a list.
+ * @head: Address of the first node of a list.
+ * @n: Integer to insert in the new node.
+ * Return: Address of the new node.
+ **/
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-  listint_t *new;
-  listint_t *temp;
+  listint_t *demp, *demp2;
 
-  (void)temp;
-
-  new = malloc(sizeof(listint_t));
-
-  if (new == NULL)
+  demp = malloc(sizeof(listint_t));
+  if (demp == NULL)
     return (NULL);
 
-  new->n = n;
-  new->next = NULL;
-  temp = *head;
+  demp->n = n;
+  demp->next = NULL;
+
   if (*head == NULL)
     {
-      *head = new;
-    }
-  else
-    {
-      while (temp->next != NULL)
-	{
-	  temp = temp->next;
-	}
-      temp->next = new;
+      *head = demp;
+      return (demp);
     }
 
-  return (*head);
+  demp2 = *head;
+  while (demp2->next)
+    demp2 = demp2->next;
+  demp2->next = demp;
+  return (demp);
 }
