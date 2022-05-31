cc506312d534e99f76a7626017599c60004cd476
Mboko David <98537263+Meekdavid@users.noreply.github.com>
2022-05-30 19:59:54 -0700

    Crack My code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,73 @@
 #include "main.h"
 
+
+
 /**
+
  * binary_to_uint - converts a binary number to an
+
  * unsigned int.
+
  * @b: binary.
+
  *
+
  * Return: unsigned int.
+
  */
+
 unsigned int binary_to_uint(const char *b)
+  
 {
-	unsigned int ui;
-	int len, base_two;
+  
+  unsigned int ui;
+  
+  int len, base_two;
+  
 
-	if (!b)
-		return (0);
+  
+  if (!b)
+    
+    return (0);
+  
 
-	ui = 0;
+  
+  ui = 0;
+  
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
+  
+  for (len = 0; b[len] != '\0'; len++)
+    
+    ;
+  
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+  
+  for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+    
+    {
+      
+      if (b[len] != '0' && b[len] != '1')
+	
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+	  
+	  return (0);
+	  
 	}
+      
 
-	return (ui);
+      
+      if (b[len] & 1)
+	
+	{
+	  
+	  ui += base_two;
+	  
+	}
+      
+    }
+  
+
+  
+  return (ui);
+  
 }
