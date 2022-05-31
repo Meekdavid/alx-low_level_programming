2eb03c600accc03b9c62eb61f2c04a1d7fc5ee6a
Mboko David <98537263+Meekdavid@users.noreply.github.com>
2022-05-30 21:29:03 -0700

    BitManipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,53 +1,20 @@
 #include "main.h"
 
-
-
 /**
-
- * get_bit - returns the value of a bit at a given
-
- * index.
-
- * @n: unsigned long int input.
-
- * @index: index of the bit.
-
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The bit.
+ * @index: The index to get the value at - indices start at 0.
  *
-
- * Return: value of the bit.
-
+ * Return: If an error occurs - -1.
+ *         Otherwise - The value of bit at index.
  */
-
 int get_bit(unsigned long int n, unsigned int index)
-  
 {
-  
-  unsigned int i;
-  
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-  
-  if (n == 0 && index < 64)
-    
-    return (0);
-  
+	if ((n & (1 << index)) == 0)
+		return (0);
 
-  
-  for (i = 0; i <= 63; n >>= 1, i++)
-    
-    {
-      
-      if (index == i)
-	
-	{
-	  
-	  return (n & 1);
-	  
-	}
-      
-    }
-  
-
-  
-  return (-1);
-  
+	return (1);
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,47 +1,19 @@
 #include "main.h"
 
-
-
 /**
-
- * clear_bit - sets the value of a bit to 0.
-
- * at a given index.
-
- * @n: pointer of an unsigned long int.
-
- * @index: index of the bit.
-
+ * clear_bit - Sets the value of a bit at a given index to 0.
+ * @n: A pointer to the bit.
+ * @index: The index to set the value at - indices start at 0.
  *
-
- * Return: 1 if it worked, -1 if it didn't.
-
+ * Return: If an error occurs - -1.
+ *         Otherwise - 1.
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
-  
 {
-  
-  unsigned int m;
-  
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-  
-  if (index > 63)
-    
-    return (-1);
-  
+	*n &= ~(1 << index);
 
-  
-  m = 1 << index;
-  
-
-  
-  if (*n & m)
-    
-    *n ^= m;
-  
-
-  
-  return (1);
-  
+	return (1);
 }
