c72782b8540482e97886636779b662bac0d254bf
Mboko David <98537263+Meekdavid@users.noreply.github.com>
2022-11-02 00:32:03 -0700

    interpolation

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,43 +1,52 @@
 #include "search_algos.h"
 
 /**
-  * interpolation_search - Searches for a value in a sorted array
-  *                        of integers using interpolation search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the first index where the value is located.
-  *
-  * Description: Prints a value every time it is compared in the array..
-  */
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t i, l, r;
+	size_t pos, low, high;
+	double f;
 
 	if (array == NULL)
 		return (-1);
 
-	for (l = 0, r = size - 1; r >= l;)
+	low = 0;
+	high = size - 1;
+
+	while (size)
 	{
-		i = l + (((double)(r - l) / (array[r] - array[l])) * (value - array[l]));
-		if (i < size)
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		else
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
+
+		if (pos >= size)
 		{
-			printf("Value checked array[%ld] is out of range\n", i);
+			printf(" is out of range\n");
 			break;
 		}
-
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			r = i - 1;
 		else
-			l = i + 1;
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
+
+		if (array[pos] == value)
+			return ((int)pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+
+		if (low == high)
+			break;
 	}
 
-	
-return (-1);
+	return (-1);
 }
